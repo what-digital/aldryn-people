013d05e75a43e6027c98f5d2fd8dda10af0c9e1a
Vladimir Nosov <vladimir@what.digital>
2020-07-22 13:15:57 +0700

    fixed TemplateDoesNotExist

---
+++ aldryn_people/boilerplates/bootstrap3/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'aldryn_people.boilerplates.bootstrap3.apps.PeopleConfig'
---
+++ aldryn_people/boilerplates/bootstrap3/apps.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from django.apps import AppConfig
+
+
+class PeopleConfig(AppConfig):
+    name = 'aldryn_people.boilerplates.bootstrap3'
+    label = 'people_bootstrap3'
